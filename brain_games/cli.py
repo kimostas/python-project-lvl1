89cfb2ba406f1ba7c1f5e98501f75d373eab806a
Kim Stanislav <stlove.kim@gmail.com>
2021-07-26 17:58:19 +0300

    add linter

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+
 import prompt
+
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
+
 
 if __name__ == '__main__':
     main()
